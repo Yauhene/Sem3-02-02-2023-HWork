69f99b814371806bf16dddc66c22b191fa45bea5
Yauheni Aviltsau <Y.Aviltsau@Weisrussland.loc>
2023-02-03 13:40:36 +0300

    add Task19 is ready

--- Task19-Palindrome/Program.cs
+++ Task19-Palindrome/Program.cs
@@ -9,20 +9,42 @@ Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine("Ну шо? Приступим помолясь?");
-
+Console.WriteLine();
 Console.Write("Введите пятизначное число: ");
-
-
-
 
 if(int.TryParse(Console.ReadLine(), out int number))
 {
+    //Console.WriteLine("number: " + number);
     int numberLen=(int)Math.Log10(Math.Abs(number)) + 1; // Определяем разрядность введенного целого числа
-    Console.WriteLine("Разрядность введенного числа - " + numberLen);
-    if (numberlen!=5) Console.WriteLine("По-человечески же просили - П Я Т И З Н А Ч Н О Е !!!");
+    //Console.WriteLine("Разрядность введенного числа - " + numberLen);
+    if (numberLen != 5 ) Console.WriteLine("По-человечески же просили - ПЯТИЗНАЧНОЕ !!!");
+    // Console.WriteLine("number: " + number);
+    // Console.WriteLine("numberLen: " + numberLen);
+
 }
-else Console.WriteLine*("Введено не целое число");
-
-int[] digArr=new int[5];
+else Console.WriteLine("Введено не целое число");
 
 
+int newNumber=0;
+int doubleNumber=number;
+
+for(int i=5; i != 0; i--) // "Разворачиваем" число number, превращая его в newNumber
+{
+    newNumber=newNumber * 10;
+    newNumber=newNumber + (doubleNumber % 10);
+    doubleNumber=doubleNumber / 10;
+    
+    
+}
+
+Console.WriteLine($"Введенное число в 'развернутом' виде: {newNumber}");
+Console.WriteLine("----------------------------------------------------------------------------");
+
+if (number != newNumber)
+{
+    Console.WriteLine($"Введеное число  {number} не является палиндромом");
+}
+else Console.WriteLine($"Введеное число  {number} является палиндромом");
+
+Console.WriteLine("----------------------------------------------------------------------------");
+Console.WriteLine();
