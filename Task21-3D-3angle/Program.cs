d1c38d77775b3cb28ce53d1b68b17769651423f6
Yauheni Aviltsau <Y.Aviltsau@Weisrussland.loc>
2023-02-03 11:54:16 +0300

    add Task21 is ready

--- Task21-3D-3angle/Program.cs
+++ Task21-3D-3angle/Program.cs
@@ -4,7 +4,10 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Ну шо? Приступим помолясь?");
 
 int[] arrayA = new int[3];
 int[] arrayB = new int[3];
@@ -71,6 +74,17 @@ void coordInput(string a) // метод заполнения массивов к
 }
 
 coordInput("A");
-Console.WriteLine("-----------------------------------");
+coordInput("B");
+
+Console.WriteLine("----------------------------------------------------------------------------");
 Console.WriteLine($"Введены координаты XYZ для точки A: {arrayA[0]} , {arrayA[1]} , {arrayA[2]}");
 Console.WriteLine($"Введены координаты XYZ для точки B: {arrayB[0]} , {arrayB[1]} , {arrayB[2]}");
+Console.WriteLine("----------------------------------------------------------------------------");
+
+// Расчет длины отрезка по введенным координатам:
+
+double distance = Math.Sqrt(Math.Pow((arrayA[0]-arrayB[0]),2)+Math.Pow((arrayA[1]-arrayB[1]),2)+Math.Pow((arrayA[2]-arrayB[2]),2));
+distance=Math.Round(distance, 2);
+
+Console.WriteLine($"В результате длительных и мучительных вычислений наши эксперты пришли к выводу, что расстояние между указанными точками скорее всего будет равно {distance}. Скорее всего... но это не точно.");
+Console.WriteLine();
