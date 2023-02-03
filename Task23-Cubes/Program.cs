0e02b45a173702e9c678e01c0b2ab1e5a95402a8
Yauheni Aviltsau <Y.Aviltsau@Weisrussland.loc>
2023-02-03 14:03:29 +0300

    add Task23 is ready

---
+++ Task23-Cubes/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Ну шо? Приступим помолясь?");
+Console.WriteLine();
+Console.Write("Введите целое число: ");
+
+String Str="| ";
+
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    for(int i=1; i != number+1; i++)
+    {
+        Str=Str+ Math.Pow(i, 3) + " | ";
+    }
+}
+else Console.WriteLine("Введено не целое число");
+
+Console.WriteLine();
+Console.WriteLine($"Ряд значений кубов целых чисел от 1 до {number}:");
+Console.WriteLine("----------------------------------------------------------------------------");
+Console.WriteLine(Str);
+Console.WriteLine("----------------------------------------------------------------------------");
+Console.WriteLine();
